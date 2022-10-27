47876633d8bbc9aa49ab1de51f9bbf06f7c74e86
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-27 16:44:39 -0700

    Add signup, login, view profile tests

--- test_user_views.py
+++ test_user_views.py
@@ -8,6 +8,7 @@
 import os
 from re import S
 from unittest import TestCase
+from app import CURR_USER_KEY
 
 from models import db, Message, User, connect_db
 
@@ -16,7 +17,7 @@ from models import db, Message, User, connect_db
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
+# os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 # Now we can import app
 
@@ -44,9 +45,10 @@ class UserBaseViewTestCase(TestCase):
         User.query.delete()
 
         u1 = User.signup("u1", "u1@email.com", "password", None)
+        u2 = User.signup("u2", "u2@email.com", "password", None)
         db.session.flush()
 
-        db.session.add(u1)
+        db.session.add(u1, u2)
         db.session.commit()
 
         self.u1_id = u1.id
@@ -56,29 +58,115 @@ class UserBaseViewTestCase(TestCase):
 class UserAddViewTestCase(UserBaseViewTestCase):
     """All of the tests for User views/routes"""
 
-    def test_add_user(self):
-        """Test functionality for user registration / adding user to database"""
+    def test_add_user_success(self):
+        """Test functionality for user registration on success"""
 
         with app.test_client() as client:
             url = "/signup"
             resp = client.post(
                 url, data = {
-                    "username": "u2",
-                    "email": "u2@email.com",
+                    "username": "test",
+                    "email": "test@email.com",
                     "password": "password",
                     "image_url": None},follow_redirects=True)
 
             html = resp.get_data(as_text=True)
 
             #Verify that the correct user page appears after user is created
-            self.assertIn("@u2", html)
+            self.assertIn("@test", html)
+
+            #Verify that the proper response code is received after user registration
+            self.assertEqual(resp.status_code, 200)
+
+    def test_add_user_failure(self):
+        """Test functionality for user registration on failure with bad data"""
+
+        with app.test_client() as client:
+            url = "/signup"
+            resp = client.post(
+                url, data = {
+                    "username": "baduser",
+                    "email": "bademail",
+                    "password": "badpassword",
+                    "image_url": None},follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("Invalid", html)
 
             #Verify that the proper response code is received after user registration
             self.assertEqual(resp.status_code, 200)
 
 
+    def test_login_user_success(self):
+        """Test functionality for user login on success"""
+
+        with app.test_client() as client:
+            url = "/login"
+            resp = client.post(
+                url, data = {
+                    "username": "u1",
+                    "password": "password"
+                }, follow_redirects=True
+            )
+
+            html = resp.get_data(as_text=True)
+
+            #Verify that the correct user page appears after user is logged in
+            self.assertIn("@u1", html)
+
+            #Verify that the proper response code is received after user login
+            self.assertEqual(resp.status_code, 200)
 
 
+    def test_login_user_failure(self):
+        """Test functionality for user login on failure with bad data"""
+
+        with app.test_client() as client:
+            url = "/login"
+            resp = client.post(
+                url, data = {
+                    "username": "baduser",
+                    "password": "badpassword"
+                }, follow_redirects=True
+            )
+
+            html = resp.get_data(as_text=True)
+
+            #Verify that the correct user page appears after user is logged in
+            self.assertIn("Invalid credentials", html)
+
+            #Verify that the proper response code is received after user login
+            self.assertEqual(resp.status_code, 200)
 
 
+    def test_view_profile_logged_in(self):
+        """Test if user profiles can be visited if logged in"""
 
+        with app.test_client() as client:
+
+            with client.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+
+            url = f"/users/{self.u1_id}"
+            resp = client.get(url, follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u1", html)
+            self.assertIn("Messages", html)
+            self.assertIn("Following", html)
+            self.assertIn("Followers", html)
+
+
+    def test_view_profile_logged_out(self):
+        """Test if user profiles can be visited if logged out"""
+
+        with app.test_client() as client:
+
+            url = f"/users/{self.u1_id}"
+            resp = client.get(url, follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("Access unauthorized", html)
